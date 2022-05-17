7948d188e46cfba0bbac4f571bcb56516a87873f
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-05-17 07:18:54 +0200

    update init_setup

--- init_setup.sh
+++ init_setup.sh
@@ -8,10 +8,10 @@ pip install -r requirements.txt
 echo [$(date)]: "export conda environment"
 conda env export > conda.yaml
 echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
-echo [$(date)]: "END"
+#echo [$(date)]: "first commit"
+##git add .
+#git commit -m "first commit"
+#echo [$(date)]: "END"
 
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
